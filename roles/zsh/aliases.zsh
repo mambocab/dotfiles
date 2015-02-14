04e99ecdee72ebf7c3b316990c9bfc8641c397c4
Jim Witschey <jim.witschey@gmail.com>
2015-02-14 18:40:18 -0500

    from prezto to antigen! and other zsh things

--- roles/zsh/aliases.zsh
+++ roles/zsh/aliases.zsh
@@ -1,3 +1,33 @@
-alias zshconfig="exec $EDITOR ~/.zshrc"
+alias zshrc="exec $EDITOR ~/.zshrc"
 alias vimrc="exec $EDITOR ~/.vimrc"
+
 alias vi="vim"
+alias emacs='emacs -nw'
+
+alias ax=chmod a+x
+
+## get public ip address
+alias ip="curl http://icanhazip.com/"
+
+## top (via ttscoff)
+alias cpu='top -o cpu'
+alias mem='top -o rsize' # memory
+
+## ls better (via ttscoff)
+alias la="ls -aFG"
+alias ld="ls -ld"
+alias ll="ls -l"
+alias lt='ls -At1 && echo "------Oldest--"'
+alias ltr='ls -Art1 && echo "------Newest--"'
+alias ls="ls -FG"
+
+# speedtesting alias via http://osxdaily.com/2013/07/31/speed-test-command-line/
+alias speedtest='wget -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test10.zip'
+
+# up and down arrow searches for commands starting with current buffer
+bindkey '\e[A' history-beginning-search-backward
+bindkey '\e[B' history-beginning-search-forward
+
+# zsh builtins
+disable r
+autoload -U zmv
