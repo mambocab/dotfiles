066fd0f46d9bd0da719ff6b8637c79ed5720cc66
Jim Witschey <jim.witschey@gmail.com>
2015-02-14 18:40:52 -0500

    tmux/aliases -> tmux/custom-commands

--- roles/tmux/aliases.zsh
+++
@@ -1,3 +0,0 @@
-alias tmux="TERM=screen-256color-bce tmux"
-alias tmls="tmux list-session"
-alias tmat="tmux attach-session -d -t"
---
+++ roles/tmux/custom-commands.zsh
@@ -0,0 +1,10 @@
+alias tmux="TERM=screen-256color-bce tmux"
+alias tmls="tmux list-sessions"
+alias tmat="tmux attach-session -d -t"
+
+function tmss {
+    tmux list-sessions |
+    cut -f 1 -d: |
+    selecta |
+    xargs tmux switch-client -t
+}
