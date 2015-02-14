9f252bec6affe5151583c4e19aed7ef71a65cfaf
Jim Witschey <jim.witschey@gmail.com>
2015-02-14 14:35:32 -0500

    custom tmux aliases
    includes alias for proper coloring in tmux

--- roles/tmux/aliases.zsh
+++ roles/tmux/aliases.zsh
@@ -1,3 +1,3 @@
-alias t="tmux"
-alias tls="tmux list-session"
-alias tat="tmux attach-session -d -t"
+alias tmux="TERM=screen-256color-bce tmux"
+alias tmls="tmux list-session"
+alias tmat="tmux attach-session -d -t"
