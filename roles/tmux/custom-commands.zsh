f94285984c0dd30d911ff1b49aaf23d4e6103a8d
Jim Witschey <jim.witschey@gmail.com>
2016-06-16 21:08:15 -0400

    add tmux history command

--- roles/tmux/custom-commands.zsh
+++ roles/tmux/custom-commands.zsh
@@ -1,6 +1,7 @@
 alias tmux="TERM=screen-256color-bce tmux"
 alias tmls="tmux list-sessions"
 alias tmat="tmux attach-session -d -t"
+alias tmhist="tmux capture-pane -S -30000 && tmux save-buffer -"
 
 function tmss {
     tmux list-sessions |
