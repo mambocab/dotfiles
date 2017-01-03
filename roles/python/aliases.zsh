f7b09e02f0d45e67a1be383ee986a242348236bd
Jim Witschey <jim.witschey@gmail.com>
2017-01-03 10:31:20 -0500

    add scratchenv command

--- roles/python/aliases.zsh
+++ roles/python/aliases.zsh
@@ -4,3 +4,4 @@ alias mkenv2="conda create python=2 ipython pip -n "
 alias lsenv="conda info -e"
 alias wo="source activate"
 alias de="source deactivate"
+alias scratchenv='pyenv uninstall scratchenv ; pyenv virtualenv scratchenv ; pyenv activate scratchenv'
