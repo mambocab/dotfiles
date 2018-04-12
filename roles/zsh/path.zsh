45a0e127a0c23c7f97476aa2b8aefaa1d6ede8f3
Jim Witschey <jim.witschey@gmail.com>
2018-04-12 09:26:45 -0400

    add linuxbrew sbin to path

--- roles/zsh/path.zsh
+++ roles/zsh/path.zsh
@@ -2,6 +2,7 @@
 # Make sure to APPEND to the path, so that homebrewed binaries, e.g. /usr/local/bin are found first
 export PATH=$PATH:$ZSH/bin:~/bin:/usr/bin:~/.local/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/usr/texbin:~/local/bin
 
+export PATH="$HOME/.linuxbrew/sbin:$PATH"
 export PATH="$HOME/.linuxbrew/bin:$PATH"
 export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
 export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
