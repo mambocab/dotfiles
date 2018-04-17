47a3949baf74d5d5090822f7b24fabadbaf6f378
Jim Witschey <jim.witschey@gmail.com>
2018-04-17 11:06:13 -0400

    don't clobber scratchenvs

--- roles/python/aliases.zsh
+++ roles/python/aliases.zsh
@@ -5,4 +5,4 @@ alias lsenv="conda info -e"
 alias wo="pyenv activate"
 alias de="deactivate"
 alias scratchenv='pyenv uninstall scratchenv ; pyenv virtualenv 2.7.12 scratchenv ; pyenv activate scratchenv'
-alias scratchenv3='pyenv uninstall scratchenv ; pyenv virtualenv 3.6.3 scratchenv ; pyenv activate scratchenv'
+alias scratchenv3='pyenv uninstall scratchenv3 ; pyenv virtualenv 3.6.3 scratchenv3 ; pyenv activate scratchenv3'
