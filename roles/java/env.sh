13b00e659a744303f784d5a871e01e31a5c06e14
Jim Witschey <jim.witschey@gmail.com>
2019-01-16 14:34:15 -0500

    big gross update

--- roles/java/env.sh
+++
@@ -1,2 +0,0 @@
-export JAVA_HOME=/usr/bin
-echo lol
--- roles/ubuntu/alias.zsh
+++ roles/ubuntu/alias.zsh
@@ -1,2 +1,2 @@
-alias disable-internal-monitor='xrandr --output eDP1 --off'
-alias enable-internal-monitor='xrandr --output eDP1 --mode 1600x900 && xrandr --output DP1 --auto --left-of eDP1'
+alias disable-internal-monitor='xrandr --output eDP-1 --off'
+alias enable-internal-monitor='xrandr --output eDP-1 --mode 1600x900 && xrandr --output DP-1 --auto --left-of eDP-1'
