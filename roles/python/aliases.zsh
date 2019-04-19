1f577195f06e0384596f62aa0f2be5938951ae9a
Jim Witschey <jim.witschey@gmail.com>
2019-04-19 14:00:24 -0400

    migrate from conda to pyenv

--- roles/python/aliases.zsh
+++ roles/python/aliases.zsh
@@ -1,7 +1,3 @@
-# Conda environments
-alias mkenv="conda create python=3 ipython pip -n "
-alias mkenv2="conda create python=2 ipython pip -n "
-alias lsenv="conda info -e"
 alias wo="pyenv activate"
 alias de="deactivate"
 alias scratchenv='pyenv uninstall scratchenv ; pyenv virtualenv 2.7.12 scratchenv ; pyenv activate scratchenv'
