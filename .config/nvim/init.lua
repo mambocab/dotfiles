f1dfce0011ae7290f98b2d1a6c7f19e87eb81151
Jim Witschey <james_witschey@rapid7.com>
2024-05-16 21:36:45 -0400

    Add initial nvim configuration
    Mostly copied straight from The Primeagen.

---
+++ .config/nvim/init.lua
@@ -0,0 +1,149 @@
+vim.g.mapleader = ' '
+
+-- Boostrap lazy.vim.
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim"
+		},
+	},
+
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/nvim-cmp",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"j-hui/fidget.nvim",
+		},
+
+		config = function()
+			local cmp = require('cmp')
+			local cmp_lsp = require("cmp_nvim_lsp")
+			local capabilities = vim.tbl_deep_extend(
+				"force",
+				{},
+				vim.lsp.protocol.make_client_capabilities(),
+				cmp_lsp.default_capabilities())
+
+			require("fidget").setup({})
+			require("mason").setup()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"rust_analyzer",
+					"gopls",
+				},
+				handlers = {
+					function(server_name) -- default handler (optional)
+						require("lspconfig")[server_name].setup {
+							capabilities = capabilities
+						}
+					end,
+
+					["lua_ls"] = function()
+						local lspconfig = require("lspconfig")
+						lspconfig.lua_ls.setup {
+							capabilities = capabilities,
+							settings = {
+								Lua = {
+									runtime = { version = "Lua 5.1" },
+									diagnostics = {
+										globals = { "vim", "it", "describe", "before_each", "after_each" },
+									}
+								}
+							}
+						}
+					end,
+				}
+			})
+
+			local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+					['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+					['<C-y>'] = cmp.mapping.confirm({ select = true }),
+					["<C-Space>"] = cmp.mapping.complete(),
+				}),
+				sources = cmp.config.sources({
+					{ name = 'nvim_lsp' },
+					{ name = 'luasnip' }, -- For luasnip users.
+				}, {
+					{ name = 'buffer' },
+				})
+			})
+
+			vim.diagnostic.config({
+				-- update_in_insert = true,
+				float = {
+					focusable = false,
+					style = "minimal",
+					border = "rounded",
+					source = "always",
+					header = "",
+					prefix = "",
+				},
+			})
+		end
+	},
+	{
+		'tpope/vim-commentary'
+	},
+	{
+		'nvim-telescope/telescope.nvim',
+		requires = { { 'nvim-lua/plenary.nvim' } }
+	},
+	{ "nvim-treesitter/nvim-treesitter" },
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		requires = { { "nvim-treesitter/nvim-treesitter" } }
+	},
+	{
+		'eandrju/cellular-automaton.nvim' -- Just for fun.
+	}
+})
+
+vim.keymap.set('n', '<leader>q', ':quit<CR>')
+vim.keymap.set('n', '<leader>e', ':edit<CR>')
+vim.keymap.set('n', '<leader>b', ':buffer<CR>')
+vim.keymap.set('n', '<leader>w', ':write<CR>')
+vim.keymap.set('n', '<leader>s', ':split<CR>')
+vim.keymap.set('n', '<leader>v', ':vsplit<CR>')
+vim.keymap.set('n', '<leader>r', '<cmd>lua vim.lsp.buf.format()<CR>')
+vim.keymap.set('v', '<leader>r', '<cmd>lua vim.lsp.buf.format()<CR>')
+
+local telescope_builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>f', telescope_builtin.find_files, {})
+vim.keymap.set('n', '<leader>g', telescope_builtin.live_grep, {})
+vim.keymap.set('n', '<leader>b', telescope_builtin.buffers, {})
+vim.keymap.set('n', '<leader>h', telescope_builtin.help_tags, {})
+
+vim.keymap.set('n', 'gc', 'commentary')
