0540bcac8bc7bfe2e7dfb2dec056e08fc466ed6c
Jim Witschey <james_witschey@rapid7.com>
2024-05-21 14:09:41 -0400

    Adds keybindings and updates for nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -154,3 +154,17 @@ vim.keymap.set('n', '<leader>h', telescope_builtin.help_tags, {})
 vim.keymap.set('n', 'gc', 'commentary')
 
 vim.api.nvim_set_option('smartcase', true)
+
+-- Harpoon setup.
+local harpoon = require("harpoon")
+harpoon:setup()
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+-- General Harpoon access keybindings.
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
